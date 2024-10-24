5bf724fd417ee333361d227621399bf4bec8fae4
Lampower <123113309+Lampower@users.noreply.github.com>
2024-10-24 20:54:32 +0400

    base setup to backend

---
+++ Backend/Backend/Common/Configurations/DatabaseConfig.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Common.Configurations
+{
+    public static class DatabaseConfig
+    {
+        public const string SQLITE_CONNECTION_STRING = "Data Source = database.db";
+    }
+}
---
+++ Backend/Backend/Database/Models/BaseEntity.cs
@@ -0,0 +1,6 @@
+﻿namespace Backend.Database.Models
+{
+    public class BaseEntity
+    {
+    }
+}
