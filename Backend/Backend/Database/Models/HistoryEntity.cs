5b8a215ee1a4df963f084f68a6f9ad12326f2d14
Lampower <123113309+Lampower@users.noreply.github.com>
2024-10-26 01:45:09 +0400

    Fix to overwritting my project

---
+++ Backend/Backend/Database/Models/HistoryEntity.cs
@@ -0,0 +1,12 @@
+﻿namespace Backend.Database.Models
+{
+    public class HistoryEntity
+    {
+        public int Id { get; set; }
+        public int TaskId { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string? PropertyName { get; set; }
+        public string? OldValue { get; set; }
+        public string? NewValue { get; set; }
+    }
+}
