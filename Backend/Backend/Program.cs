99dbaac02b77bb6edf39f361f04268769d93d0e9
Lampower <123113309+Lampower@users.noreply.github.com>
2024-10-27 16:15:16 +0400

    unmerge merge

--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,3 +1,4 @@
+using Backend.Common.Services;
 using Backend.Database;
 using System.Reflection;
 
@@ -22,6 +23,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DatabaseContext>();
+builder.Services.AddScoped<CSVParserToDb>();
 
 // Create init to services
 
